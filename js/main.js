890ede92142b5ea7808670e17d45ff25ed48f02f
Wiszniewski <Adrian.Wiszniewski@ricoh-usa.com>
2017-11-22 11:31:11 -0500

    fixed to work with mobile and desktop

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
-$("#wikiSearch").focus();
-  var api = 'https://en.wikipedia.org/w/api.php?action=query&format=json&prop=pageimages&titles=&generator=search&&gsrlimit=7&gsrsearch=';
+  $("#wikiSearch").focus();
+  var api = 'https://en.wikipedia.org/w/api.php?action=query&format=json&prop=pageimages&titles=&generator=search&mobileformat=&gsrlimit=7&gsrsearch=';
   var cb = '&callback=?';
   var dataServer;
+  var windowWidth = $(window).width();
 
   $("#randomButton").click(function(){
-    $(".results").html('<object data="https://en.wikipedia.org/wiki/Special:Random"/>');
+    $(".results").html('<object data="https://en.m.wikipedia.org/wiki/Special:Random"/>');
   });
 
-
   $("form").submit(function(e) {
-   e.preventDefault();
+    e.preventDefault();
     $(".results").empty();
     var input = $("#wikiSearch").val();
 
@@ -23,10 +23,15 @@ $("#wikiSearch").focus();
       },
       contentType: "application/json; charset=utf-8",
       success: function(data) {
-         dataServer = data.query.pages;
+        dataServer = data.query.pages;
         $.each(dataServer, function(k, v) {
           var $title = $("<div>").text(v.title);
-          var $link = $("<object data='https://en.wikipedia.org/?curid='/>").html($title);
+            if(windowWidth < 700 ){
+              var $link = $("<object data='https://en.m.wikipedia.org/?curid='/>").html($title);
+            }
+            else {
+              var $link = $("<object data='https://en.wikipedia.org/?curid='/>").html($title);
+            }
           $link.attr('data', $link.attr('data') + v.pageid);
           $(".results").append($link).fadeIn(1000);
         });
@@ -34,4 +39,4 @@ $("#wikiSearch").focus();
     });
   });
 
-            });
+});
